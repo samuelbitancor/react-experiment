1f42da9ee4bd8cb138047c22f090710fd9eecf5c
samuelbitancor <92839955+samuelbitancor@users.noreply.github.com>
2022-03-30 12:36:59 +0800

    updated reactDOM render module

--- src/index.js
+++ src/index.js
@@ -59,5 +59,9 @@ class Game extends React.Component {
   }
 }
 
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
 
 reportWebVitals();
